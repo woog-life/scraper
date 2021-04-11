94e28233ab232a22c446aac6b11e9985a2d7e26e
Torben Carstens <github@carstens.tech>
2021-04-11 18:04:05 +0200

    Add air information

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ WOOG_UUID = os.getenv("LARGE_WOOG_UUID")
 API_KEY = os.getenv("API_KEY")
 
 WATER_INFORMATION = NewType("WaterInformation", Tuple[str, float])
+AIR_INFORMATION = NewType("AirInformation", Tuple[str, float])
 
 
 def create_logger(name: str, level: int = logging.DEBUG) -> logging.Logger:
@@ -75,6 +76,37 @@ def get_tag_text_from_xml(xml: Union[BeautifulSoup, Tag], name: str, conversion:
     return conversion(tag.text)
 
 
+def get_air_information(soup: BeautifulSoup) -> Optional[AIR_INFORMATION]:
+    logger = create_logger(inspect.currentframe().f_code.co_name)
+
+    air_temperature_tag = soup.find("Air_Temperature")
+    logger.debug(f"air_temperature_tag: {air_temperature_tag}")
+    if not air_temperature_tag:
+        logger.error(f"air_Temperature not present in {soup}")
+        return
+
+    try:
+        temperature = get_tag_text_from_xml(air_temperature_tag, "value", float)
+    except ValueError:
+        logger.error("value_tag was not of type float")
+        return
+
+    if not temperature:
+        logger.error(f"temperature was None in air_temperature value tag ({air_temperature_tag})")
+        return
+
+    try:
+        iso_time: int = get_tag_text_from_xml(air_temperature_tag, "ts",
+                                              lambda x: datetime.fromtimestamp(int(x) / 1000).isoformat())
+    except ValueError:
+        logger.exception("ts_tag is not valid", exc_info=True)
+        return
+
+    # noinspection PyTypeChecker
+    # at this point pycharm doesn't think that the return type can be optional despite the many empty returns beforehand
+    return iso_time, temperature
+
+
 def get_water_information(soup: BeautifulSoup) -> Optional[WATER_INFORMATION]:
     logger = create_logger(inspect.currentframe().f_code.co_name)
 
@@ -106,12 +138,14 @@ def get_water_information(soup: BeautifulSoup) -> Optional[WATER_INFORMATION]:
     return iso_time, temperature
 
 
-def send_data_to_backend(water_information: WATER_INFORMATION) -> Tuple[Optional[requests.Response], str]:
+def send_data_to_backend(water_information: WATER_INFORMATION, air_information: AIR_INFORMATION) -> Tuple[Optional[requests.Response], str]:
     logger = create_logger(inspect.currentframe().f_code.co_name)
     path = BACKEND_PATH.format(WOOG_UUID)
     url = "/".join([BACKEND_URL, path])
 
     water_timestamp, water_temperature = water_information
+    air_timestamp, air_temperature = air_information
+
     headers = {"X-ApiKey": API_KEY}
     data = {"temperature": water_temperature, "time": water_timestamp}
     logger.debug(f"Send {data} to {url} with headers {headers}")
@@ -137,12 +171,19 @@ def main() -> Tuple[bool, str]:
     soup = parse_website_xml(content)
 
     water_information = get_water_information(soup)
+    air_information = get_air_information(soup)
+
     if not water_information:
         message = f"Couldn't retrieve water information from {soup}"
         logger.error(message)
         return False, message
 
-    response, generated_backend_url = send_data_to_backend(water_information)
+    if not air_information:
+        message = f"Couldn't retrieve air information from {soup}"
+        logger.error(message)
+        return False, message
+
+    response, generated_backend_url = send_data_to_backend(water_information, air_information)
 
     if not response or not response.ok:
         message = f"Failed to put data ({water_information}) to backend: {generated_backend_url}"
